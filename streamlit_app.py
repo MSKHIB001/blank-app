3cf2375f4222026deea4be47ee345dbcf4f67504
MSKHIB001 <MSKHIB001@myuct.ac.za>
2025-05-22 09:47:59 +0000

    my streamlit app

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,20 @@
 import streamlit as st
 
-st.title("🎈 My new app")
+st.title("🎈 My new Streamlit app")
+st.header("Full of Joy")
 st.write(
     "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
 )
+name= st.text_input("Enter Name:")
+studymethod = st.selectbox("StudyMethod" ,("Crammer","Pre-Planner","Memory Box"))
+button =st.button("Diva?")      
+
+
+if button:
+    with st.spinner("Loading your diva status"):
+        if studymethod =="Crammer":
+            st.write(f"{name}, You ain't no diva")
+        elif studymethod =="Memory Box":
+            st.write(f"{name}, You are almost a diva")
+        elif studymethod == "Pre-Planner":
+            st.write(f"{name},You is a diva")
